183e3d9ccb74efa949daafa74b83a6b12f0460e1
rajithaeyee <rajithaeye@gmail.com>
2018-12-17 00:37:20 +0530

    added toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Auxx from './../../hoc/Auxx';
 import './Layout.css';
+import Toolbar from './../Navigation/Toolbar/Toolbar';
+
 const layout = (props) => {
 
     return (
         <Auxx>
+            <Toolbar/>
             <div>Toolbar,SideDrawer,Backdrop</div>
             <main className="Content">
                 {props.children}
